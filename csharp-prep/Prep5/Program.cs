9807e364cf81d3b90472e0f2f9316759a2c8a503
angelmitch3 <gal23033@byui.edu>
2024-01-14 02:51:57 -0500

    Update #5 Assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,47 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void DisplayWelcome()
     {
-        DisplayWelcomeMessage();
-
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
-
-        int squaredNumber = SquareNumber(userNumber);
-
-        DisplayResult(userName, squaredNumber);
-    }
-
-    static void DisplayWelcomeMessage()
-    {
-        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Welcome to the Program!");
     }
 
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
-
         return name;
     }
 
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-
-        return number;
+         int number = int.Parse(Console.ReadLine());    
+         return number;
     }
 
     static int SquareNumber(int number)
     {
-        int square = number * number;
-        return square;
+        return number * number;
     }
 
-    static void DisplayResult(string name, int square)
+    static void DisplayResult(string name, int squaredNumber)
     {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
+
+    static void Main()
+    {
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
+    }
+    
 }
