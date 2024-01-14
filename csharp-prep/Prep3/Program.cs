de754a878efa1938e2f203aba6b0d76bbc6b9cb1
angelmitch3 <gal23033@byui.edu>
2024-01-14 01:45:21 -0500

    #3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        // Step 1: Ask the user for the magic number.
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // Step 2: Ask the user for a guess.
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
+
+        // Step 3: Determine if the user needs to guess higher or lower next time, or tell them if they guessed it.
+        if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        // Step 4: Add a loop that keeps looping as long as the guess does not match the magic number.
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        // Step 5: Instead of having the user supply the magic number, generate a random number from 1 to 100.
+        Random rand = new Random();
+        magicNumber = rand.Next(1, 101);
+
+        Console.WriteLine("A new magic number has been generated. Let's play again!");
+        userGuess = -1; // Reset the user guess.
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    
+
+
     }
 }
