2eee4b6de5d0abbe7dab1fb3a727dd5c2ff3837c
angelmitch3 <gal23033@byui.edu>
2024-01-14 02:21:35 -0500

    Update # 4 Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -12,7 +12,8 @@ class Program
         do
         {
             Console.Write("Enter number: ");
-            number = Convert.ToInt32(Console.ReadLine());
+            string answer = Console.ReadLine();
+             number = int.Parse(answer);
             if (number != 0)
             {
                 numbers.Add(number);
@@ -28,7 +29,7 @@ class Program
         Console.WriteLine("The sum is: " + sum);
 
         // Step 3: Compute the average of the numbers in the list.
-        double average = (double)sum / numbers.Count;
+        float average = (float)sum / numbers.Count;
         Console.WriteLine("The average is: " + average);
 
         // Step 4: Find the maximum, or largest, number in the list.
