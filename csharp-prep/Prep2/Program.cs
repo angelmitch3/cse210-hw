8f4819c46a6dc6338c2091299569550e17077f71
angelmitch3 <gal23033@byui.edu>
2024-01-14 01:30:28 -0500

    #2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("Enter your grade percentage: ");
+        double grade = Convert.ToDouble(Console.ReadLine());
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine("Your letter grade is: " + letter);
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't be discouraged. Study harder and Better luck next time!");
+        }
+    
     }
 }
