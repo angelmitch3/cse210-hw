b29eb4894ab2c8d1579b1225e140a556098dc426
angelmitch3 <michael_gtmc@yahoo.com>
2024-01-28 00:53:57 -0500

    Updated encapsulation

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,3 +1,5 @@
+//Added features to add a new verse, select a scripture verse, pick a random verse and to practice
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,15 @@ public class Program
         AddScripture(new Reference("Moses", 1, 39), "For behold, this is my work and my glory—to bring to pass the immortality and eternal life of man.");
         AddScripture(new Reference("Moses", 7, 18), "And the Lord called his people Zion, because they were of one heart and one mind, and dwelt in righteousness; and there was no poor among them.");
         AddScripture(new Reference("Abraham", 3, 22, 23), "And God saw these souls that they were good, and he stood in the midst of them, and he said: These I will make my rulers; for he stood among those that were spirits, and he saw that they were good; and he said unto me: Abraham, thou art one of them; thou wast chosen before thou wast born.");
+        AddScripture(new Reference("Matthew", 5, 14, 16), "Ye are the light of the world. A city that is set on an hill cannot be hid. Neither do men light a candle, and put it under a bushel, but on a candlestick; and it giveth light unto all that are in the house. Let your light so shine before men, that they may see your good works, and glorify your Father which is in heaven.");
+        AddScripture(new Reference("Alma", 32, 21), "And now as I said concerning faith—faith is not to have a perfect knowledge of things; therefore if ye have faith ye hope for things which are not seen, which are true.");
+        AddScripture(new Reference("D&C", 19, 16, 19), "For behold, I, God, have suffered these things for all, that they might not suffer if they would repent; But if they would not repent they must suffer even as I; Which suffering caused myself, even God, the greatest of all, to tremble because of pain, and to bleed at every pore, and to suffer both body and spirit—and would that I might not drink the bitter cup, and shrink—Nevertheless, glory be to the Father, and I partook and finished my preparations unto the children of men.");
+        AddScripture(new Reference("Moroni", 10, 4, 5), "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may know the truth of all things.");
 
         while (true)
         {
-            Console.WriteLine("\nType 'add' to add a new verse, 'select' to select a verse, 'random' to pick a random verse, 'practice' to practice, or 'quit' to exit.");
+            Console.WriteLine("\nPlease type the word based on the selected option below: ");
+            Console.WriteLine("\n1. 'add' to add a new verse\n2. 'select' to select a scripture verse\n3. 'random' to pick a random verse\n4. 'practice' to practice\n5. 'quit' to exit.");
             string input = Console.ReadLine();
 
             if (input.ToLower() == "quit")
